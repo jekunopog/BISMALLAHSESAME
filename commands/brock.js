40706c8b7d7ea20b6659d0b09b3cadbf18a51c33
jekunopog <75117203+jekunopog@users.noreply.github.com>
2021-06-02 19:49:10 +1000

    Update brock.js

--- commands/brock.js
+++ commands/brock.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "brock",
-    aliases: ["purge"],
+    aliases: ["broc"],
     description: "brock",
     execute(message, args) {
       return message.channel.send("brock is halal").catch(console.error)
